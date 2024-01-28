40c90e0a4887b1c98d3e31d9a0a8e1d9b540e9a7
rahulfordev <rahulforsms@gmail.com>
2024-01-28 20:50:53 +0600

    deplicate number and word remove from array

--- duplicate.js
+++ duplicate.js
@@ -1,8 +1,9 @@
-const number = [1, 5, 3, 8, 7, 3, 5, 1];
+const number = [1, 5, 3, 8, 7, 3, 5, 1]; // number
+const userName = ["Rahul", "Rohim", "Rahul", "Korim", "Rohim", "Sabur"]; // string
 let uniqueNumber = [];
 
-for (let i = 0; i < number.length; i++) {
-  const element = number[i];
+for (let i = 0; i < userName.length; i++) {
+  const element = userName[i];
   const index = uniqueNumber.indexOf(element);
   if (index == -1) {
     uniqueNumber.push(element);
