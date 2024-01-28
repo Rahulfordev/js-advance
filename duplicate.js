edbc4e12d162b7e74deeade3e361aa97132d3d88
rahulfordev <rahulforsms@gmail.com>
2024-01-28 20:25:04 +0600

    unique number find

--- duplicate.js
+++ duplicate.js
@@ -1,2 +1,11 @@
 const number = [1, 5, 3, 8, 7, 3, 5, 1];
+let uniqueNumber = [];
 
+for (let i = 0; i < number.length; i++) {
+  const element = number[i];
+  const index = uniqueNumber.indexOf(element);
+  if (index == -1) {
+    uniqueNumber.push(element);
+  }
+}
+console.log(uniqueNumber);
