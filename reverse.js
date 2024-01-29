f1ba6a12e497522cf87666ee4fb87d4c7357dd94
rahulfordev <rahulforsms@gmail.com>
2024-01-29 15:22:30 +0600

    reverse using for, for of, push

--- reverse.js
+++ reverse.js
@@ -7,5 +7,33 @@ function reverseString(str) {
   return reverse;
 }
 const string = "Hello bor, how are you?";
-const reult = reverseString(string);
-console.log(reult);
+const number = [7, 6, 5, 4, 3, 2, 1];
+const reult = reverseString(number);
+// console.log(reult);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// using for of loop
+const reverse = [];
+for (const num of numbers) {
+  reverse.unshift(num);
+}
+// console.log(reverse);
+
+// using for loop
+const reverseFor = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  const element = numbers[i];
+  reverseFor.unshift(element);
+}
+console.log(reverseFor);
+
+// using for loop with push
+const rev_rev_numbers = [];
+for (let i = numbers.length - 1; i >= 0; i--) {
+  const num = numbers[i];
+  // console.log(num);
+  rev_rev_numbers.push(num);
+}
+// console.log(rev_rev_numbers);
