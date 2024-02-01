c5bc8697e580c0ab1c7ba3a922ba1183a4faa106
rahulfordev <rahulforsms@gmail.com>
2024-02-01 12:02:39 +0600

    fiboncci

---
+++ fibonacchi.js
@@ -0,0 +1,17 @@
+// let fibo = [0, 1];
+// for (let i = 2; i <= 12; i++) {
+//   fibo[i] = fibo[i - 1] + fibo[i - 2];
+//   console.log(fibo[i], fibo[i - 1], fibo[i - 2]);
+// }
+
+// console.log(fibo);
+
+// function fibonacchi(n) {
+//   let fibo = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     fibo[i] = fibo[i - 1] + fibo[i - 2];
+//   }
+//   return fibo;
+// }
+// const fiboResult = fibonacchi(12);
+// console.log(fiboResult);
