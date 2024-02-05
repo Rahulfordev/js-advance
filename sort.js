1919fc710c2dbbec4112dd53b81dff2596e60387
rahulfordev <rahulforsms@gmail.com>
2024-02-06 00:03:36 +0600

    name sort

--- sort.js
+++ sort.js
@@ -7,5 +7,27 @@ const numbers = [4, 7, 12, 8, 43, 6, 1];
 const numbers_asc = [...numbers].sort((a, b) => a - b);
 const numbers_dsc = [...numbers].sort((a, b) => b - a);
 
-console.log(numbers_asc);
-console.log(numbers_dsc);
+// console.log(numbers_asc);
+// console.log(numbers_dsc);
+
+const person = [
+  { name: "Md Rahul", age: 20 },
+  { name: "Abir Azam", age: 24 },
+  { name: "Johir Ali", age: 16 },
+  { name: "Al hassan", age: 23 },
+  { name: "Mr. Y", age: 22 },
+];
+
+// const personAge = person.sort((a, b) => a.age - b.age);
+// console.log(personAge);
+
+const personName = person.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;
+  } else if (a.name < b.name) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
+console.log(personName);
