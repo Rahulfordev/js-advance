b16148757b1a1217bc05d4535867ce559c8181c7
rahulfordev <rahulforsms@gmail.com>
2024-02-07 11:33:45 +0600

    problem solve 2

---
+++ problemSolve/problem1.js
@@ -0,0 +1,30 @@
+// problem 1
+var createHelloWorld = function () {
+  return function (...args) {
+    return "Hello World";
+  };
+};
+
+const f = createHelloWorld();
+console.log(f());
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  return function () {
+    // return (n = n + 1); //start 11 ->
+    return n++; // start -> 10
+  };
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
