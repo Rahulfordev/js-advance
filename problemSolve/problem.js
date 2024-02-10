ec2fbd3bde7fc33ca4a4454b28aef9017d957c44
rahulfordev <rahulforsms@gmail.com>
2024-02-10 11:58:40 +0600

    problem 4

--- problemSolve/problem.js
+++ problemSolve/problem.js
@@ -62,3 +62,27 @@ console.log(result.notToBe(null));
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
+
+// problem 4
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let i = 0;
+  return {
+    increment: () => init + ++i,
+    decrement: () => init + --i,
+    reset: () => {
+      i = 0;
+      return init;
+    },
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
